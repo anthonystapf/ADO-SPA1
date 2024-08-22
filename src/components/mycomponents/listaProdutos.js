ed194f169d0da339eb0f3cb10d0da2fe24bda733
Anthony Stapf <125614058+anthonystapf@users.noreply.github.com>
2024-08-21 21:45:24 -0300

    Update listaProdutos.js

--- src/components/mycomponents/listaProdutos.js
+++ src/components/mycomponents/listaProdutos.js
@@ -9,38 +9,38 @@ const ListaProdutos = [
     "id": 2,
     "title": "The Beast Within",
     "description": "O livro é narrado pelos olhos de uma menina de 10 anos...",
-    "imageUrl": "https://topdezfilmes.de/wp-content/uploads/2024/08/The-Beast-Within-691x1024.jpg"
+    imageUrl: "https://topdezfilmes.de/wp-content/uploads/2024/08/The-Beast-Within-691x1024.jpg"
     },
     {
     "id": 3,
     "title": "Jackpot",
     "description": "Em um futuro próximo, foi criada a Grande Loteria...",
-    "imageUrl": "https://topdezfilmes.de/wp-content/uploads/2024/08/jackpot.jpg"
+    imageUrl: "https://topdezfilmes.de/wp-content/uploads/2024/08/jackpot.jpg"
     },
     {
     "id": 4,
     "title": "Como Vender a Lua",
     "description": "Chamada para consertar a imagem pública da instituição...",
-    "imageUrl": "https://topdezfilmes.de/wp-content/uploads/2024/08/fly_me_to_the_moon_ver2.jpg"
+    imageUrl: "https://topdezfilmes.de/wp-content/uploads/2024/08/fly_me_to_the_moon_ver2.jpg"
     },
     {
     
     "id": 5,
     "title": "Twisters",
     "description": "Em Twisters, Kate Cooper é uma ex-caçadora de tempestades...",
-    "imageUrl": "https://topdezfilmes.de/wp-content/uploads/2024/08/twisters_ver3.jpg"
+    imageUrl: "https://topdezfilmes.de/wp-content/uploads/2024/08/twisters_ver3.jpg"
     },
     {
     "id": 6,
     "title": "Dentro da Mente de um Cachorro",
     "description": "Embarque em uma viagem encantadora pelo mundo canino...",
-    "imageUrl": "https://topdezfilmes.de/wp-content/uploads/2024/08/Inside-the-Mind-of-a-Dog-768x960.jpg"
+    imageUrl: "https://topdezfilmes.de/wp-content/uploads/2024/08/Inside-the-Mind-of-a-Dog-768x960.jpg"
     },
     {
     "id": 7,
     "title": "MaXXXine",
     "description": "Em MAXXXINE, ambientada durante os anos 80, a trama...",
-    "imageUrl": "https://topdezfilmes.de/wp-content/uploads/2024/08/maxxxine_ver2.jpg"
+    imageUrl: "https://topdezfilmes.de/wp-content/uploads/2024/08/maxxxine_ver2.jpg"
     },
     {
     "id": 8,
@@ -52,26 +52,26 @@ const ListaProdutos = [
     "id": 9,
     "title": "Savi",
     "description": "Savi, uma simples dona de casa, tenta uma fuga...",
-    "imageUrl": "https://topdezfilmes.de/wp-content/uploads/2024/07/savi-683x1024.jpg"
+    imageUrl: "https://topdezfilmes.de/wp-content/uploads/2024/07/savi-683x1024.jpg"
     
     },
     {
     "id": 10,
     "title": "Bad Boys: Até o Fim",
     "description": "Os Bad Boys favoritos do mundo estão de volta, mas desta...",
-    "imageUrl": "https://topdezfilmes.de/wp-content/uploads/2024/07/Bad-Boys-Ate-o-Fim.webp"
+    imageUrl: "https://topdezfilmes.de/wp-content/uploads/2024/07/Bad-Boys-Ate-o-Fim.webp"
     },
     {
       "id": 11,
       "title": "Planeta dos Macacos: O Reinado",
       "description": "A trama de ‘Planeta dos Macacos: O Reinado’ é ambientada vários anos...",
-      "imageUrl": "https://topdezfilmes.de/wp-content/uploads/2024/07/Planeta-dos-MacacosO-Reinado.webp"
+      imageUrl: "https://topdezfilmes.de/wp-content/uploads/2024/07/Planeta-dos-MacacosO-Reinado.webp"
     },
     {
     "id": 12,
     "title": "O Exorcismo",
     "description": "Anthony Miller (Russell Crowe) é um ator com um passado sombrio...",
-    "imageUrl": "https://topdezfilmes.de/wp-content/uploads/2024/07/exorcism.jpg"
+    imageUrl: "https://topdezfilmes.de/wp-content/uploads/2024/07/exorcism.jpg"
     }
 ];
 
